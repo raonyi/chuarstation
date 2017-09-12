9280062f06b11e23f8137c035b8de7dac19c2659
raonyi <raonyi@gmail.com>
2017-09-12 06:58:32 +0000

    Some chuardeploy.sh improvements.

--- chuardeploy.sh
+++ chuardeploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Chuarstation deployment file - R. 05-09-17
+# Chuarstation deployment file - R. 12/09/17
 # Tested in: Raspbian Stretch Lite 2017-09-07 on Raspberry Pi 3 Model B
 #
 # NOTE: Script to be run as root
@@ -12,8 +12,6 @@
 # Main vars
 CH_ROOT="/opt/chuarstation"
 CH_LOGFILE="${CH_ROOT}/chuardeploy.log"
-# Compilation vars
-
 
 #
 # FUNCTIONS
@@ -42,8 +40,9 @@ function ch_deploy_sysupdate_raspbian {
 }
 
 function ch_deploy_prepare {  
-  # Prepare chuarstation base tree
   ch_log "*** Preparing environment..."
+
+  # Prepare chuarstation base tree
   mkdir ${CH_ROOT} &>> ${CH_LOGFILE}
   mkdir ${CH_ROOT}/bin &>> ${CH_LOGFILE}
   mkdir ${CH_ROOT}/etc &>> ${CH_LOGFILE}
@@ -58,32 +57,36 @@ function ch_deploy_sdl2 {
   CH_SDL2_CMAKE=""
   CH_SDL2_MAKE="-j4"
   
-  # Implementation
+  # Prepare
   ch_log "***"
   ch_log "*** Deploying SDL2..."
   ch_log "***"
   mkdir -p ${CH_ROOT}/src/sdl2
   cd ${CH_ROOT}/src/sdl2
+
   # Get sources
   ch_log "*** SDL2 - Getting sources"
   wget "${CH_SDL2_SOURCES}"
   tar -xzf SDL2-2.0.5.tar.gz
   rm SDL2-2.0.5.tar.gz
   cd SDL2-2.0.5/
+
   # Prepare cmake environment
   ch_log "*** SDL2 - cmake"
   mkdir build;cd build
   cmake ../ ${CH_SDL2_CMAKE} &>> ${CH_LOGFILE}
+
   # Compile
   ch_log "*** SDL2 - make"
   make ${CH_SDL2_MAKE} &>> ${CH_LOGFILE}
+
   # Install system-wide
   ch_log "*** SDL2 - make install"
   make install &>> ${CH_LOGFILE}
-  # Link shared libraries
   ch_log "*** SDL2 - ldconfig"
   ldconfig &>> ${CH_LOGFILE}
   cd ../../../..
+
   ch_log "*** SDL2 deploy finished."
 }
 
@@ -93,31 +96,35 @@ function ch_deploy_sfmlpi { # Mickelson's SFML-PI
   CH_SFMLPI_CMAKE="-DSFML_RPI=1 -DEGL_INCLUDE_DIR=/opt/vc/include -DEGL_LIBRARY=/opt/vc/lib/libEGL.so -DGLES_INCLUDE_DIR=/opt/vc/include -DGLES_LIBRARY=/opt/vc/lib/libGLESv2.so -DGLES2_LIBRARY=/opt/vc/lib/libGLESv2.so"
   CH_SFMLPI_MAKE="-j4"
 
-  # Implementation
+  # Prepare
   ch_log "***"
   ch_log "*** Deploying sfml-pi..."
   ch_log "***"
   mkdir src/sfml-pi;cd src/sfml-pi/
+
   # Get sources
   ch_log "*** sfml-pi - Getting sources..."
   wget "${CH_SFMLPI_SOURCES}"
   unzip master.zip > /dev/null # Prevent undesired output to screen
   rm master.zip
   cd sfml-pi-master
+
   # Prepare cmake environment
   ch_log "*** sfml-pi - cmake"
   mkdir build;cd build
   cmake ../ ${CH_SFMLPI_CMAKE} &>> ${CH_LOGFILE}
   ch_log "*** sfml-pi - make"
+
   # Compile
   make ${CH_SFMLPI_MAKE} &>> ${CH_LOGFILE}
   ch_log "*** sfml-pi - make install"
+
   # Install system-wide
   make install &>> ${CH_LOGFILE}
   ch_log "*** sfml-pi - ldconfig"
-  # Link shared libraries
   ldconfig &>> ${CH_LOGFILE}
   cd ${CH_ROOT}
+
   ch_log "*** sfml-pi deploy finished."
 }
 
@@ -127,11 +134,12 @@ function ch_deploy_ffmpeg {
   CH_FFMPEG_CONFIGURE="--enable-mmal --enable-shared"
   CH_FFMPEG_MAKE="-j4"
   
-  # Implementation
+  # Prepare
   ch_log "***"
   ch_log "*** Deploying FFmpeg..."
   ch_log "***"
-  # FFmpeg
+
+  # Get sources
   cd src
   mkdir ffmpeg;cd ffmpeg
   ch_log "*** FFmpeg - Getting sources..."
@@ -139,15 +147,21 @@ function ch_deploy_ffmpeg {
   unzip master.zip > /dev/null # Prevent undesired output to screen
   rm master.zip
   cd FFmpeg-master/
+
+  # Build
   ch_log "*** FFmpeg - ./configure"
   ./configure ${CH_FFMPEG_CONFIGURE} &>> ${CH_LOGFILE}
   ch_log "*** FFmpeg - make"
   make ${CH_FFMPEG_MAKE} &>> ${CH_LOGFILE}
+  
+  # Install system-wide
   ch_log "*** FFmpeg - make install"
   make install &>> ${CH_LOGFILE}
   ch_log "*** FFmpeg - ldconfig"
   ldconfig &>> ${CH_LOGFILE}
+
   cd ${CH_ROOT}
+
   ch_log "*** FFmpeg deploy finished."
 }
 function ch_deploy_attract {
@@ -155,27 +169,31 @@ function ch_deploy_attract {
   CH_ATTRACT_SOURCES="https://github.com/mickelson/attract/archive/master.zip"
   CH_ATTRACT_MAKE="-j4 USE_GLES=1"
   
-  # Implementation
+  # Prepare
   ch_log "***"
   ch_log "*** Deploying Attract-Mode..."
   ch_log "***"
   cd src
   mkdir attract;cd attract
+
   # Get sources
   ch_log "*** Attract-Mode - Getting sources"
   wget ${CH_ATTRACT_SOURCES}
   unzip master.zip > /dev/null # Prevent undesired output to screen
   rm master.zip
   cd attract-master/
-  # Compile
+
+  # Build
   ch_log "*** Attract-Mode - make"
   make ${CH_ATTRACT_MAKE} &>> ${CH_LOGFILE}
-  # Copy initial config in system shared folder and binary to CH_ROOT/bin
+
+  # Install
   ch_log "*** Attract-mode - Set config and binary..."
   cp -va config /usr/local/share/attract &>> ${CH_LOGFILE}
   cp -v attract ${CH_ROOT}/bin &>> ${CH_LOGFILE}
   cd ${CH_ROOT}
-  #to start for first time: ./attract -f DejaVuSans
+  #to start for first time append "-f DejaVuSans" when running ./attract
+
   ch_log "*** Attract-mode deploy finished."
 }
 
@@ -185,34 +203,38 @@ function ch_deploy_retroarch {
   CH_RETROARCH_CONFIGURE="--disable-xmb --enable-dispmanx --enable-opengles --disable-pulse --disable-oss --enable-neon --enable-floathard"
   CH_RETROARCH_MAKE="-j4"
   
-  # Implementation
+  # Prepare
   ch_log "***"
   ch_log "*** Deploying Retroarch..."
   ch_log "***"
   cd ${CH_ROOT}/src
   mkdir retroarch;cd retroarch
+
   # Get sources
   ch_log "*** Retroarch - Getting sources..."
   wget ${CH_RETROARCH_SOURCES}
   unzip master.zip > /dev/null # Prevent undesired output to screen
   rm -f master.zip
   cd RetroArch-master/
-  # Configure and compile
+
+  # Build
   ch_log "*** Retroarch - ./configure"
   ./configure ${CH_RETROARCH_CONFIGURE} &>> ${CH_LOGFILE}
   ch_log "*** Retroarch - make"
   make ${CH_RETROARCH_MAKE} &>> ${CH_LOGFILE}
-  # Move binary file to CH_ROOT/bin
+
+  # Install
   cp -v retroarch ${CH_ROOT}/bin/
   cd ${CH_ROOT}
+
   ch_log "*** Retroarch deploy finished."
 }
 
 function ch_printusage {
   echo "** Usage:"
-  echo "\tchuardeploy.sh all\tDeploy everything"
-  echo "\tchuardeploy.sh <component>\tInstall specific component. Not yet implemented."
-  echo "\tchuardeploy.sh -h\tThis help."
+  echo -e "chuardeploy.sh all\tDeploy everything"
+  echo -e "chuardeploy.sh <item>\tInstall specific component. Not yet implemented."
+  echo -e "chuardeploy.sh -h\tThis help."
 }
 
 #
